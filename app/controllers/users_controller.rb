168f07d807814cb9f91fbb00a059b81364694295
Isaiah <ijandicos90@gmail.com>
2022-08-23 06:16:25 -0400

    Completed Lab

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+    def show
+    user = User.find(params[:id])
+    render json: user, include: :items 
+    end
+    private
+
+    def render_not_found_response
+        render json: { error: "Item not found" }, status: :not_found
+      end
+    
 end
